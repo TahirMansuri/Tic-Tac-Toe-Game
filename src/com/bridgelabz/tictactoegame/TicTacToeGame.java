6c501ffb4f6718024f3b92b2b09527e55f645a06
TahirMansuri <tprocks786@gmail.com>
2022-01-03 20:29:39 +0530

    MODIFIED: Set Initial Playing board for Game

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -1,9 +1,10 @@
 package com.bridgelabz.tictactoegame;
 
 public class TicTacToeGame {
-
+    //Playing Board Array for Locations
     private static char[] playingBoard = new char[10];
 
+    //UC 1 Method for initializing board position
     private static void initializeBoard() {
         for(int i=1; i<10; i++) {
             playingBoard[i]=' ';
