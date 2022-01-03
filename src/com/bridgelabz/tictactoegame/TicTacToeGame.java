51bb28b39609ed69bf918932bdd6d747062526b1
TahirMansuri <tprocks786@gmail.com>
2022-01-03 20:27:21 +0530

    MODIFIED: Set Initial Playing board for Game

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -1,9 +1,21 @@
 package com.bridgelabz.tictactoegame;
 
 public class TicTacToeGame {
+
+    private static char[] playingBoard = new char[10];
+
+    private static void initializeBoard() {
+        for(int i=1; i<10; i++) {
+            playingBoard[i]=' ';
+        }
+    }
+
     public static void main(String[] args) {
 
         //Tic Tac Toe Game Development
         System.out.println("Welcome to Tic Tac Toe Game Simulator.");
+
+        //Initializing the Playing Board boxes
+        initializeBoard();
     }
 }
